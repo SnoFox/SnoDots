bda48731025d81cdb721b0d1236f572d003b0f31
Josh Johnson <snofox@snofox.net>
2017-09-04 17:18:43 -0700

    Allow for overlays

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ cd $(dirname ${BASH_SOURCE})/
 OS="null"
 KERNEL="null"
 ARCH="null"
+DEFSFILE="defs.tml"
 
 find_linux_distro() {
 	[ -e /etc/os-release ] && relfile="/etc/os-release" || relfile="/usr/lib/os-release"
@@ -64,19 +65,41 @@ find_exe_suffix() {
 }
 
 install_tasks() {
-	if [ $# -eq 0 ]; then
-		echo 'No tasks requested; installing "base"'
-		set -- base $@
-	fi
 	find_os
 	find_arch
 	find_exe_suffix
 	BINNAME=homemaker_${KERNEL}_${ARCH}${SUFFIX}
+	ARGS=()
+	if [ $DRY -eq 1 ]; then
+		ARGS+=("-nocmds")
+		ARGS+=("-nolinks")
+	fi
+	if [ $VERBOSE -eq 1 ]; then
+		ARGS+=("-verbose")
+	fi
 	for i in "$@"; do
-		homemaker/$BINNAME -task $i -variant $OS base.tml .
+		HM_OVERLAY=${OVERLAY} homemaker/$BINNAME ${ARGS[@]} -variant $OS -task install_$i $DEFSFILE .
+		HM_OVERLAY=${OVERLAY} homemaker/$BINNAME ${ARGS[@]} -variant $OVERLAY -task setup_$i $DEFSFILE .
 	done
 }
 
+queue_install() {
+	PACKAGES+=($1)
+}
+
+list_packages() {
+	awk -F. '/^\[tasks\.(setup_|install_|default)/ { next } /^\[tasks./ { print substr($2, 1, length($2)-1) }' $DEFSFILE
+}
+
+list_overlays() {
+	awk -F_ '/^\[tasks\.setup_.+__.+]/ { print substr($4, 1, length($4)-1) }' $DEFSFILE|sort|uniq
+}
+
+verbose_log() {
+	if [ $VERBOSE -eq 1 ]; then
+		echo $@
+	fi
+}
 
 show_usage() {
 	cat <<HELP
@@ -84,30 +107,70 @@ Usage: $0 <option>
 
 One of:
 	-h		Show this message
-	-i		Install packages
+	-i		Install packages (implies -s)
+	-o		Overlay (default: $OVERLAY)
 	-s		Setup dotfiles
 HELP
 }
 
-while getopts "his" opts; do
+# Plan
+DRY=0
+VERBOSE=0
+PACKAGES=()
+OVERLAY="sno"
+while getopts ":dhi:Llo:sv" opts; do
 	case $opts in
+		d)
+			DRY=1
+			;;
 		h)
 			show_usage
 			exit 0
 			;;
 		i)
-			shift
-			install_tasks $@
+			queue_install $OPTARG
+			;;
+		l)
+			list_packages
 			exit 0
+			;;
+		L)
+			list_overlays
+			exit 0
+			;;
+		o)
+			OVERLAY=$OPTARG
 			;;
 		s)
-			echo "Not yet implemented"
+			echo "Not yet implemented on its own"
 			exit 0
 			;;
+		v)
+			VERBOSE=1
+			;;
+		:)
+			echo "Error: -$OPTARG requires an argument"
+			show_usage
+			exit 1
+			;;
 		*)
-			echo "Unknown option: $OPTARG"
+			echo "Error: Unknown option -$OPTARG"
 			show_usage
 			exit 1
 			;;
 	esac
 done
+
+# Do it
+if [ ${#PACKAGES[@]} -eq 0 ]; then
+	echo "Nothing to do. Exiting..."
+	exit 0
+fi
+
+if [ $DRY -eq 1 ]; then
+	echo "Doing dry run..."
+fi
+
+verbose_log "Set overlay to $OVERLAY"
+
+install_tasks ${PACKAGES[@]}
