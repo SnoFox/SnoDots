cf17a370a15bf71501d44cd429fe2c73d0d0059f
Josh Johnson <jjohnson@okta.com>
2019-04-15 16:19:36 -0700

    i3: Mas config.

--- i3/startup.sh
+++ i3/startup.sh
@@ -2,3 +2,11 @@
 
 # For Cape and other Thinkpads
 xinput set-prop "TPPS/2 IBM TrackPoint" "libinput Accel Speed" -0.4
+
+set -x
+screens=$(xrandr|egrep '^e?DP-(.+) connected'|wc -l)
+if [ $screens -gt 2 ]; then
+  ${HOME}/.screenlayout/alternate.sh
+else
+  ${HOME}/.screenlayout/default.sh
+fi
