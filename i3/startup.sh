e2f1b347da2d0bb5b834877888dbf83c7910369f
Josh Johnson <jjohnson@okta.com>
2021-03-08 12:39:29 -0800

    i3 updates

--- i3/startup.sh
+++ i3/startup.sh
@@ -2,13 +2,14 @@
 
 # For Cape and other Thinkpads
 xinput set-prop "TPPS/2 IBM TrackPoint" "libinput Accel Speed" -0.4
-xset dpms 0 0 0
+#xset dpms 0 0 0
 xset s off
+xset dpms 0 0 300
 
 set -x
 screens=$(xrandr|egrep '^e?DP-(.+) connected'|wc -l)
 if [ $screens -gt 2 ]; then
-  ${HOME}/.screenlayout/alternate.sh
+  ${HOME}/.screenlayout/docked.sh
 else
-  ${HOME}/.screenlayout/default.sh
+  ${HOME}/.screenlayout/internal.sh
 fi
