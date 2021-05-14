70f734d1749eb1af25c688ef9c7578a22111f507
Josh Johnson <snofox@snofox.net>
2021-05-14 01:42:25 -0700

    Fix git/kubectl install

--- support/install-kubectl.sh
+++ support/install-kubectl.sh
@@ -3,7 +3,7 @@ set -euo pipefail
 curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -sLO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(<kubectl.sha256) kubectl" | sha256sum --check --status
-mkdir -p ${HOME}/.bin
+mkdir -p ${HOME}/.local/bin
 chmod 755 kubectl
-mv kubectl ${HOME}/.bin
+mv kubectl ${HOME}/.local/bin
 rm kubectl.sha256
