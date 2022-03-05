84828c9d62f84797c23efea74d113e127d11c0bc
Josh Johnson <snofox@snofox.net>
2022-03-05 15:21:38 -0800

    Prefer local bin over system bin

--- support/install-kubectl.sh
+++ support/install-kubectl.sh
@@ -3,7 +3,6 @@ set -euo pipefail
 curl -sLO "https://dl.k8s.io/release/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl"
 curl -sLO "https://dl.k8s.io/$(curl -L -s https://dl.k8s.io/release/stable.txt)/bin/linux/amd64/kubectl.sha256"
 echo "$(<kubectl.sha256) kubectl" | sha256sum --check --status
-mkdir -p ${HOME}/.local/bin
 chmod 755 kubectl
 mv kubectl ${HOME}/.local/bin
 rm kubectl.sha256
